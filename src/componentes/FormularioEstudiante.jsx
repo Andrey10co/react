34c154a7ba4525d4aefb6fdd0273faecd8b45f93
AndreyCG <andreycogo@unisabana.edu.co>
2023-04-17 14:00:46 -0500

    validacion de campo

--- src/componentes/FormularioEstudiante.jsx
+++ src/componentes/FormularioEstudiante.jsx
@@ -40,3 +40,48 @@ export const FormularioEstudiante = ({ agregar }) => {
         </>
     )
 }
+
+const onSubmit = (userInfo) => console.log(userInfo);
+
+const messages = {
+    req: "Este campo es obligatorio",
+    nom: "Requiere minimo 3 caracteres",
+    codigo: "Debes tener un id de minimo 6 y maximo 9 numeros"
+   };
+
+   const patterns = {
+    name: /^[A-Za-z]+$/i,
+    mail: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+    phone: /^[0-9]+$/i
+   };
+
+
+<form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-group ">
+                    <label htmlFor="id">ID Estudiante</label>
+                    <input type="id" className="form-control" id="id" placeholder="Ingrese id" value={id} {...register("id"), { required:
+                                                                messages.required,
+                                                                minLength: {
+                                                                value: 6,
+                                                                message: messages.phone
+                                                                },
+                                                                maxLength: {
+                                                                value: 9,
+                                                                message: messages.phone
+                                                                },
+                                                                pattern: {
+                                                                value: patterns.phone,
+                                                                message: messages.phone
+                                                                }}} onChange={(event) => setId(event.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="nombre">Nombre</label>
+                    <input type="text" className="form-control" id="nombre" placeholder="nombre" value={nombre} {...register("nombre"), { required: true }} onChange={(event) => setNombre(event.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="semestre">Semestre</label>
+                    <input type="text" className="form-control" id="semestre" placeholder="semestre" value={semestre} {...register("semestre"), { required: true }} onChange={(event) => setSemestre(event.target.value)} />
+                </div>
+
+                <button type="submit" className="btn btn-primary">Registrar</button>
+            </form>
